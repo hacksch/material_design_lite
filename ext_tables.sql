c00c05fbb48ecb708733a21136ed21c4a3b24185
Martin Tepper <martin.tepper@aoe.com>
2016-04-25 15:16:20 +0200

    set default to 12

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-    tx_material_design_lite_grid_default int(11) DEFAULT '8' NOT NULL,
+    tx_material_design_lite_grid_default int(11) DEFAULT '12' NOT NULL,
 	tx_material_design_lite_grid_desktop int(11) DEFAULT '0' NOT NULL,
 	tx_material_design_lite_grid_tablet int(11) DEFAULT '0' NOT NULL,
 	tx_material_design_lite_grid_phone int(11) DEFAULT '0' NOT NULL,
